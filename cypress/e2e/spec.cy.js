cfe741b6e813ae276e541d04b59ae02ed80d961e
Arden Ranta <arranta20@gmail.com>
2024-01-16 10:46:54 -0800

    non-api test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('App component', () => {
+  it('displays non-api reliant elements on page load', () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 200,
+      fixture: 'urls',
+    }).as('fetchURLs');
+    cy.get('h1').should('contain', 'URL Shortener');
+    cy.get('form').should('be.visible');
+    cy.get('input').should('have.value', '');
+    cy.get('form').children().should('have.length', 2);
+  });
 })
